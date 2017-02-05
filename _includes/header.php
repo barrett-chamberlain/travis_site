4d580ce29b0463bf2d2cc88736cbfe68302e4105
Barrett Chamberlain <wiredchamberlain@gmail.com>
2017-02-04 18:32:40 -0800

    includes added

---
+++ _includes/header.php
@@ -0,0 +1,51 @@
+<!-- HEADER-->
+<header>
+    <div class="header-topbar">
+        <div class="container">
+            <div class="topbar-left pull-left">
+                <div class="email"><a href="#"><i class="topbar-icon fa fa-envelope-o"></i><span>hello.shinway@gmail.com</span></a></div>
+                <div class="hotline"><a href="#"><i class="topbar-icon fa fa-phone"></i><span>+84 909 015 345</span></a></div>
+            </div>
+            <div class="topbar-right pull-right">
+                <div class="socials"><a href="#" class="facebook"><i class="fa fa-facebook"></i></a><a href="#" class="google"><i class="fa fa-google-plus"></i></a><a href="#" class="twitter"><i class="fa fa-twitter"></i></a><a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a><a href="#" class="blog"><i class="fa fa-rss"></i></a><a href="#" class="dribbble"><i class="fa fa-dribbble"></i></a></div>
+                <div class="group-sign-in"><a href="login.html" class="login">login</a><a href="register.html" class="register">register</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="header-main homepage-01">
+        <div class="container">
+            <div class="header-main-wrapper">
+                <div class="navbar-heade">
+                    <div class="logo pull-left"><a href="index.html" class="header-logo"><img src="assets/images/logo-color-1.png" alt=""/></a></div>
+                    <button type="button" data-toggle="collapse" data-target=".navigation" class="navbar-toggle edugate-navbar"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                </div>
+                <nav class="navigation collapse navbar-collapse pull-right">
+                    <ul class="nav-links nav navbar-nav">
+                        <li class="dropdown active">
+                        	<a href="#" class="main-menu">Home</a>
+                        </li>
+                        <li class="dropdown">
+                        	<a href="profile.html" class="main-menu">Profile
+                        	</a>
+                        </li>
+                        <li class="dropdown">
+                        	<a href="referrals.html" class="main-menu">Referrals
+                        	</a>
+                        </li>
+                        <li class="dropdown">
+                        	<a href="methods.html" class="main-menu">Methods
+                        	</a>
+                        </li>
+                        <li><a href="contact.html" class="main-menu">Contact</a></li>
+                        <li class="button-search"><p class="main-menu"><i class="fa fa-search"></i></p></li>
+                        <div class="nav-search hide">
+                            <form><input type="text" placeholder="Search" class="searchbox"/>
+                                <button type="submit" class="searchbutton fa fa-search"></button>
+                            </form>
+                        </div>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ _includes/main_slider.php
@@ -0,0 +1,28 @@
+<div class="content"><!-- SLIDER BANNER-->
+                <div class="section slider-banner set-height-top">
+                    <div class="slider-item">
+                        <div class="slider-1">
+                                <a href="profiles.html"><img src="assets/images/slider/home-page-1.jpg" class="img-responsive"></a>
+                                <!-- <img src="assets/images/slider/home-page-1.jpg"> -->
+                        </div>
+                    </div>
+                    <div class="slider-item">
+                        <div class="slider-2">
+                                <a href="referrals.html"><img src="assets/images/slider/home-page-2.jpg" class="img-responsive"></a>
+                                <!-- <img src="assets/images/slider/home-page-1.jpg"> -->
+                        </div>
+                    </div>
+                    <div class="slider-item">
+                        <div class="slider-3">
+                                <a href="methods.html"><img src="assets/images/slider/home-page-3.jpg" class="img-responsive"></a>
+                                <!-- <img src="assets/images/slider/home-page-1.jpg"> -->
+                        </div>
+                    </div>
+                    <div class="slider-item">
+                        <div class="slider-4">
+                                <a href="contact.html"><img src="assets/images/slider/home-page-4.jpg" class="img-responsive"></a>
+                                <!-- <img src="assets/images/slider/home-page-1.jpg"> -->
+                        </div>
+                    </div><!--slider-item-->
+            </div><!--section slider-banner set-height-top-->
+        </div><!--content-->
